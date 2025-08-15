b448f661a6459e48a67c743ae43a159329bcee3c
talibDennis <joseph.dennis@triosstudent.com>
2025-08-14 22:53:08 -0400

    Added asciiArt 'Java Buddy'

--- src/main/java/JavaBuddyCLI.java
+++ src/main/java/JavaBuddyCLI.java
@@ -13,7 +13,24 @@ public class JavaBuddyCLI {
     public static void main(String[] args) {
         loadGlossary();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to JavaBuddy CLI! Type a Java term to learn about it, or type 'exit' to quit.");
+        String[] asciiArt = {
+            "       __                     ____            __    __",
+            "      / /___ __   ______ _   / __ )__  ______/ /___/ /_  __",
+            " __  / / __ `/ | / / __ `/  / __  / / / / __  / __  / / / /",
+            "/ /_/ / /_/ /| |/ / /_/ /  / /_/ / /_/ / /_/ / /_/ / /_/ /",
+            "\\____/\\__,_/ |___/\\__,_/  /_____/\\__,_/\\__,_/\\__, /\\__, /",
+            "                                                  /____/"
+        };
+
+        for (String line : asciiArt) {
+            System.out.println(line);
+            try {
+                Thread.sleep(400); // 400 milliseconds delay
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println("\nWelcome to JavaBuddy CLI! Type a Java term to learn about it (like class or polymorphism), or type 'exit' to quit.");
 
         while (true) {
             System.out.print("\nEnter a Java term: ");
@@ -28,6 +45,7 @@ public class JavaBuddyCLI {
                 Map<String, String> entry = glossary.get(input);
                 System.out.println("Definition: " + entry.get("definition"));
                 System.out.println("Example: " + entry.get("example"));
+                System.out.println("Code Example: " + entry.get("code example"));
             } else {
                 System.out.println("Sorry, that term was not found in the glossary.");
             }
